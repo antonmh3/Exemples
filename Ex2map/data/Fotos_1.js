79dfa1f4d5523beda576bf5e47eb2eac29f38275
antonmh3 <antonmh2000@gmail.com>
2022-02-23 20:05:24 +0100

    map add

---
+++ Ex2map/data/Fotos_1.js
@@ -0,0 +1,9 @@
+var json_Fotos_1 = {
+"type": "FeatureCollection",
+"name": "Fotos_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "photo": "C:\\Users\\Usuario\\Desktop\\WEBGIS 2\\Sandbox\\Exemple webgis\\PXL_20210428_112303523.jpg", "filename": "PXL_20210428_112303523", "directory": "C:\\Users\\Usuario\\Desktop\\WEBGIS 2\\Sandbox\\Exemple webgis", "altitude": 77.21, "direction": 218.0, "longitude": "1.1480444444444444", "latitude": "41.10288888888889", "timestamp": "2021-04-28T13:23:03.000" }, "geometry": { "type": "Point", "coordinates": [ 1.148044444444444, 41.102888888888891 ] } },
+{ "type": "Feature", "properties": { "photo": "C:\\Users\\Usuario\\Desktop\\WEBGIS 2\\Sandbox\\Exemple webgis\\PXL_20210428_112313300.jpg", "filename": "PXL_20210428_112313300", "directory": "C:\\Users\\Usuario\\Desktop\\WEBGIS 2\\Sandbox\\Exemple webgis", "altitude": 77.21, "direction": 315.0, "longitude": "1.1480444444444444", "latitude": "41.10288888888889", "timestamp": "2021-04-28T13:23:13.000" }, "geometry": { "type": "Point", "coordinates": [ 1.148044444444444, 41.102888888888891 ] } }
+]
+}
